a79ffda30c87c3f76372efb22df0030c484d3e65
Davy Kamanzi <72526783+DavyK17@users.noreply.github.com>
2021-02-24 02:18:18 +0300

    Creating Mortal Kombat player list

---
+++ assets/js/player-list-mk.js
@@ -0,0 +1,88 @@
+/* BIRTH DATE FORMATTING */
+const dobOptions = { day: "numeric", month: "long", year: "numeric" };
+
+/* PLAYERS */
+const players = {
+    /*
+    // Template
+    0000000: {
+        playerId: 0000000,
+        playerImg: "/assets/img/players/0000000.jpg",
+
+        playerName: "Full name",
+        pseudonym: "Pseudonym",
+
+        team: "",
+        teamShorthand: "",
+
+        birthDate: new Date("YYYY-MM-DD").toLocaleDateString("en-KE", dobOptions),
+        displayBirthDate: true,
+
+        platform: "Platform",
+        onlineId: "Online ID",
+
+        mainChar: "",
+        otherChars: [""],
+
+        facebook: "https://facebook.com/",
+        twitter: "https://twitter.com/",
+        instagram: "https://instagram.com/",
+        youtube: "https://youtube.com/",
+        twitch: "https://twitch.tv/",
+
+        stats: {
+            circuit: {
+                // Totals
+                totalTourneys: 17,
+                totalSetsWon: 87,
+                totalSetsLost: 40,
+                totalMatchesWon: 211,
+                totalMatchesLost: 149,
+                totalHighestFinish: "2nd",
+
+                // Season One
+                isSeason1: true,
+                // isFinalistSeason1: ,
+
+                // s1RankF: "",
+                s1Rank: "",
+                s1Points: ,
+                s1Tourneys: ,
+                s1SetsWon: ,
+                s1SetsLost: ,
+                s1MatchesWon: ,
+                s1MatchesLost: ,
+                s1HighestFinish: "",
+                s1HFtourney: "",
+            },
+            sfn: {
+                currentRank: "",
+                currentRankDiff: "Up/Down/Same",
+                // previousRank: ,
+
+                setsWon: ,
+                setsLost: ,
+                matchesWon: 1,
+                matchesLost: ,
+
+                currentWinStreak: ,
+                longestWinStreak: ,
+                currentLossStreak: ,
+                longestLossStreak: ,
+                highestRank: "",
+                hrEdition: ,
+
+                lastSFN: ,
+                lastResult: "",
+                lastResultScore: "",
+                lastResultOpp: "",
+                lastResultOppId: ,
+            },
+        },
+    },
+    */
+};
+
+
+/* EXPORTING PROFILES */
+export { players };
