0f78c0654b2d8857873747225c21e1fe81290f08
Davy Kamanzi <72526783+DavyK17@users.noreply.github.com>
2021-02-24 00:07:25 +0300

    Creating Mortal Kombat character list

---
+++ assets/js/character-list-mk.js
@@ -0,0 +1,443 @@
+/* CHARACTERS */
+const characters = {
+    // Baraka
+    baraka: {
+        // Profile
+        name: "Baraka",
+        sex: "male",
+        image: "/assets/img/characters/mk11/baraka.png",
+    },
+
+    // Cassie Cage
+    cassie: {
+        // Profile
+        name: "Cassie Cage",
+        sex: "female",
+        image: "/assets/img/characters/mk11/cassie.png",
+    },
+    get cassieCage() {
+        return this.cassie;
+    },
+    get "cassie-cage" () {
+        return this.cassie;
+    },
+    get "cassie cage" () {
+        return this.cassie;
+    },
+
+    // Cetrion
+    cetrion: {
+        // Profile
+        name: "Cetrion",
+        sex: "female",
+        image: "/assets/img/characters/mk11/cetrion.png",
+    },
+
+    // D'Vorah
+    dvorah: {
+        // Profile
+        name: "D'Vorah",
+        sex: "female",
+        image: "/assets/img/characters/mk11/dvorah.png",
+    },
+    get "d'vorah" () {
+        return this.dvorah;
+    },
+
+    // Erron Black
+    erronBlack: {
+        // Profile
+        name: "Erron Black",
+        sex: "male",
+        image: "/assets/img/characters/mk11/erron-black.png",
+    },
+    get erron() {
+        return this.erronBlack;
+    },
+    get "erron-black" () {
+        return this.erronBlack;
+    },
+    get "erron black" () {
+        return this.erronBlack;
+    },
+
+    // Frost
+    frost: {
+        // Profile
+        name: "Frost",
+        sex: "female",
+        image: "/assets/img/characters/mk11/frost.png",
+    },
+
+    // Fujin
+    fujin: {
+        // Profile
+        name: "Fujin",
+        sex: "male",
+        image: "/assets/img/characters/mk11/fujin.png",
+    },
+
+    // Geras
+    geras: {
+        // Profile
+        name: "Geras",
+        sex: "male",
+        image: "/assets/img/characters/mk11/geras.png",
+    },
+
+    // Jacqui Briggs
+    jacqui: {
+        // Profile
+        name: "Jacqui Briggs",
+        sex: "male/female/ambiguous",
+        image: "/assets/img/characters/mk11/.png",
+    },
+    get jacquiBriggs() {
+        return this.jacqui;
+    },
+    get "jacqui-briggs" () {
+        return this.jacqui;
+    },
+    get "jacqui briggs" () {
+        return this.jacqui;
+    },
+
+    // Jade
+    jade: {
+        // Profile
+        name: "Jade",
+        sex: "female",
+        image: "/assets/img/characters/mk11/jade.png",
+    },
+
+    // Jax Briggs
+    jax: {
+        // Profile
+        name: "Jax Briggs",
+        sex: "male",
+        image: "/assets/img/characters/mk11/jax.png",
+    },
+    get jaxBriggs() {
+        return this.jax;
+    },
+    get "jax-briggs" () {
+        return this.jax;
+    },
+    get "jax briggs" () {
+        return this.jax;
+    },
+
+    // Johnny Cage
+    johnny: {
+        // Profile
+        name: "Johnny Cage",
+        sex: "male",
+        image: "/assets/img/characters/mk11/johnny.png",
+    },
+    get johnnyCage() {
+        return this.johnny;
+    },
+    get "johnny-cage" () {
+        return this.johnny;
+    },
+    get "johnny cage" () {
+        return this.johnny;
+    },
+
+    // The Joker
+    joker: {
+        // Profile
+        name: "The Joker",
+        sex: "male",
+        image: "/assets/img/characters/mk11/joker.png",
+    },
+    get theJoker() {
+        return this.joker;
+    },
+    get "the-joker" () {
+        return this.joker;
+    },
+    get "the joker" () {
+        return this.joker;
+    },
+
+    // Kabal
+    kabal: {
+        // Profile
+        name: "Kabal",
+        sex: "male",
+        image: "/assets/img/characters/mk11/kabal.png",
+    },
+
+    // Kano
+    kano: {
+        // Profile
+        name: "Kano",
+        sex: "male",
+        image: "/assets/img/characters/mk11/kano.png",
+    },
+
+    // Kitana
+    kitana: {
+        // Profile
+        name: "Kitana",
+        sex: "female",
+        image: "/assets/img/characters/mk11/kitana.png",
+    },
+
+    // Kollector
+    kollector: {
+        // Profile
+        name: "Kollector",
+        sex: "male",
+        image: "/assets/img/characters/mk11/kollector.png",
+    },
+
+    // Kotal Kahn
+    kotalKahn: {
+        // Profile
+        name: "Kotal Kahn",
+        sex: "male",
+        image: "/assets/img/characters/mk11/kotal-kahn.png",
+    },
+    get kotal() {
+        return this.kotalKahn;
+    },
+    get "kotal-kahn" () {
+        return this.kotalKahn;
+    },
+    get "kotal kahn" () {
+        return this.kotalKahn;
+    },
+
+    // Kung Lao
+    kungLao: {
+        // Profile
+        name: "Kung Lao",
+        sex: "male",
+        image: "/assets/img/characters/mk11/kung-lao.png",
+    },
+    get "kung-lao" () {
+        return this.kungLao;
+    },
+    get "kung lao" () {
+        return this.kungLao;
+    },
+
+    // Liu Kang
+    liuKang: {
+        // Profile
+        name: "Liu Kang",
+        sex: "male",
+        image: "/assets/img/characters/mk11/liu-kang.png",
+    },
+    get "liu-kang" () {
+        return this.liuKang;
+    },
+    get "liu kang" () {
+        return this.liuKang;
+    },
+
+    // Mileena
+    mileena: {
+        // Profile
+        name: "Mileena",
+        sex: "female",
+        image: "/assets/img/characters/mk11/mileena.png",
+    },
+
+    // Nightwolf
+    nightwolf: {
+        // Profile
+        name: "Nightwolf",
+        sex: "male",
+        image: "/assets/img/characters/mk11/nightwolf.png",
+    },
+
+    // Noob Saibot
+    noobSaibot: {
+        // Profile
+        name: "Noob Saibot",
+        sex: "male",
+        image: "/assets/img/characters/mk11/noob-saibot.png",
+    },
+    get noob() {
+        return this.noobSaibot;
+    },
+    get "noob-saibot" () {
+        return this.noobSaibot;
+    },
+    get "noob saibot" () {
+        return this.noobSaibot;
+    },
+
+    // Raiden
+    raiden: {
+        // Profile
+        name: "Raiden",
+        sex: "male",
+        image: "/assets/img/characters/mk11/raiden.png",
+    },
+
+    // Rain
+    rain: {
+        // Profile
+        name: "Rain",
+        sex: "male",
+        image: "/assets/img/characters/mk11/rain.png",
+    },
+
+    // John Rambo
+    rambo: {
+        // Profile
+        name: "John Rambo",
+        sex: "male",
+        image: "/assets/img/characters/mk11/rambo.png",
+    },
+    get johnRambo() {
+        return this.rambo;
+    },
+    get "john-rambo" () {
+        return this.rambo;
+    },
+    get "john rambo" () {
+        return this.rambo;
+    },
+
+    // RoboCop
+    robocop: {
+        // Profile
+        name: "RoboCop",
+        sex: "male",
+        image: "/assets/img/characters/mk11/robocop.png",
+    },
+    get "robo-cop" () {
+        return this.robocop;
+    },
+
+    // Scorpion
+    scorpion: {
+        // Profile
+        name: "Scorpion",
+        sex: "male",
+        image: "/assets/img/characters/mk11/scorpion.png",
+    },
+
+    // Shang Tsung
+    shangTsung: {
+        // Profile
+        name: "Shang Tsung",
+        sex: "male",
+        image: "/assets/img/characters/mk11/shang-tsung.png",
+    },
+    get "shang-tsung" () {
+        return this.shangTsung;
+    },
+    get "shang tsung" () {
+        return this.shangTsung;
+    },
+
+    // Shao Kahn
+    shaoKahn: {
+        // Profile
+        name: "Shao Kahn",
+        sex: "male",
+        image: "/assets/img/characters/mk11/shao-kahn.png",
+    },
+    get "shao-kahn" () {
+        return this.shaoKahn;
+    },
+    get "shao kahn" () {
+        return this.shaoKahn;
+    },
+
+    // Sheeva
+    sheeva: {
+        // Profile
+        name: "Sheeva",
+        sex: "female",
+        image: "/assets/img/characters/mk11/sheeva.png",
+    },
+
+    // Sindel
+    sindel: {
+        // Profile
+        name: "Sindel",
+        sex: "female",
+        image: "/assets/img/characters/mk11/sindel.png",
+    },
+
+    // Skarlet
+    skarlet: {
+        // Profile
+        name: "Skarlet",
+        sex: "female",
+        image: "/assets/img/characters/mk11/skarlet.png",
+    },
+
+    // Sonya Blade
+    sonya: {
+        // Profile
+        name: "Sonya Blade",
+        sex: "female",
+        image: "/assets/img/characters/mk11/sonya.png",
+    },
+    get sonyaBlade() {
+        return this.sonya;
+    },
+    get "sonya-blade" () {
+        return this.sonya;
+    },
+    get "sonya blade" () {
+        return this.sonya;
+    },
+
+    // Sub-Zero
+    subZero: {
+        // Profile
+        name: "Sub-Zero",
+        sex: "male",
+        image: "/assets/img/characters/mk11/sub-zero.png",
+    },
+    get "sub-zero" () {
+        return this.subZero;
+    },
+    get "sub zero" () {
+        return this.subZero;
+    },
+
+    // The Terminator
+    terminator: {
+        // Profile
+        name: "The Terminator",
+        sex: "male",
+        image: "/assets/img/characters/mk11/terminator.png",
+    },
+    get theTerminator() {
+        return this.terminator;
+    },
+    get "the-terminator" () {
+        return this.terminator;
+    },
+    get "the terminator" () {
+        return this.terminator;
+    },
+    get "terminator-t-800" () {
+        return this.terminator;
+    },
+    get "terminator t-800" () {
+        return this.terminator;
+    },
+
+    /*
+    template: {
+        // Profile
+        name: "",
+        sex: "male/female/ambiguous",
+        image: "/assets/img/characters/mk11/.png",
+    },
+    */
+}
+
+/* EXPORTING CHARACTERS */
+export { characters };
