490a7a679f22599e7f21564137c40f9203d13a3f
Davy Kamanzi <72526783+DavyK17@users.noreply.github.com>
2021-05-03 17:45:29 +0300

    Additional config for jekyll-tagging

---
+++ _plugins/ext.rb
@@ -0,0 +1,5 @@
+# _plugins/ext.rb allows you to define custom plugins and/or require
+# plugins that otherwise wouldn't work with Jekyll
+
+# Activate jekyll-tagging: https://github.com/pattex/jekyll-tagging
+require 'jekyll/tagging'
