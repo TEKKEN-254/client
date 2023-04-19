87f5dda2aeae982849724fafb84ada184a275c9b
Davy Kamanzi <davykamanzi@gmail.com>
2023-04-19 23:53:44 +0300

    Updating

--- assets/js/helper-functions.js
+++ assets/js/helper-functions.js
@@ -1,72 +1,59 @@
-/* DUPLICATE OBJECT */
-const duplicateObject = (object, duplicate) => {
-    for (let key in object) {
-        duplicate[key] = object[key];
-    }
-}
+/* NUMBER TO WORDS FUNCTION || Adapted from https://gist.github.com/ForbesLindesay/5467742 */
+const numToWords = (num) => {
+	const ones = [
+		"",
+		"one",
+		"two",
+		"three",
+		"four",
+		"five",
+		"six",
+		"seven",
+		"eight",
+		"nine",
+		"ten",
+		"eleven",
+		"twelve",
+		"thirteen",
+		"fourteen",
+		"fifteen",
+		"sixteen",
+		"seventeen",
+		"eighteen",
+		"nineteen",
+	];
+	const tens = ["", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"];
+	const numString = num.toString();
 
-/* GET GAME FUNCTION */
-const getGame = () => {
-    let url = window.location.href;
-    if (url.includes("/circuit/tekken") || url.includes("/fight-night")) {
-        return "tekken";
-    } else if (url.includes("/circuit/mk")) {
-        return "mk";
-    }
-}
+	if (num < 0) throw new Error("Negative numbers are not supported.");
+	if (num === 0) return "zero";
 
-/* NUMBER TO WORDS FUNCTION || Source: https://gist.github.com/ForbesLindesay/5467742 */
-function numToWords(num) {
-    var ones = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine',
-        'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen',
-        'seventeen', 'eighteen', 'nineteen'
-    ];
-    var tens = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty',
-        'ninety'
-    ];
+	// 1 to 20
+	if (num < 20) return ones[num];
+	if (numString.length === 2) return tens[numString[0]] + " " + ones[numString[1]];
 
-    var numString = num.toString();
+	// 100 and more
+	if (numString.length == 3) {
+		if (numString[1] === "0" && numString[2] === "0") return ones[numString[0]] + " hundred";
+		else return ones[numString[0]] + " hundred and " + convert(+(numString[1] + numString[2]));
+	}
 
-    if (num < 0) throw new Error('Negative numbers are not supported.');
-
-    if (num === 0) return 'zero';
-
-    //the case of 1 - 20
-    if (num < 20) {
-        return ones[num];
-    }
-
-    if (numString.length === 2) {
-        return tens[numString[0]] + ' ' + ones[numString[1]];
-    }
-
-    //100 and more
-    if (numString.length == 3) {
-        if (numString[1] === '0' && numString[2] === '0')
-            return ones[numString[0]] + ' hundred';
-        else
-            return ones[numString[0]] + ' hundred and ' + convert(+(numString[1] + numString[2]));
-    }
-
-    if (numString.length === 4) {
-        var end = +(numString[1] + numString[2] + numString[3]);
-        if (end === 0) return ones[numString[0]] + ' thousand';
-        if (end < 100) return ones[numString[0]] + ' thousand and ' + convert(end);
-        return ones[numString[0]] + ' thousand ' + convert(end);
-    }
-}
+	if (numString.length === 4) {
+		const end = +(numString[1] + numString[2] + numString[3]);
+		if (end === 0) return ones[numString[0]] + " thousand";
+		if (end < 100) return ones[numString[0]] + " thousand and " + convert(end);
+		return ones[numString[0]] + " thousand " + convert(end);
+	}
+};
 
 /* CAPITALIZE FUNCTION */
-const capitalize = str => {
-    if (typeof str !== "string") return "";
-    return str.charAt(0).toUpperCase() + str.slice(1);
-}
+const capitalize = (str) => {
+	if (typeof str !== "string") return "";
+	return str.charAt(0).toUpperCase() + str.slice(1);
+};
 
 /* DEFAULT OBJECT */
-const helperFunctions = {
-    duplicateObject,
-    getGame,
-    numToWords,
-    capitalize
-}
+const helperFunctions = { numToWords, capitalize };
+
+/* EXPORT */
 export default helperFunctions;
